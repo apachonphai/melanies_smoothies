bb8b907879c029586815d08a24cb63dc167ab29c
apachonphai <apachonphai@networkdistribution.com>
2026-06-17 11:45:13 -0500

    Fix SQL insert statement formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,8 +72,7 @@ if ingredients_list:
     
 #    st.write(ingredients_string)
 insert_stmt = f"""INSERT INTO SMOOTHIES.PUBLIC.ORDERS (ingredients, name_on_order, order_ts)
-VALUES ('{ingredients_string}', '{name_on_order}', CURRENT_TIMESTAMP())"""
-
+VALUES ('{ingredients_string}', '{name_on_order}', CURRENT_TIMESTAMP())
 
     
     time_to_insert = st.button('Submit Order')
