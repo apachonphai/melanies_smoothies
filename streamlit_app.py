ef8367b414ad0aa3e60e63cd17e425e834993de4
apachonphai <apachonphai@networkdistribution.com>
2026-06-17 11:46:12 -0500

    Add SQL insert statement for smoothie orders

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,6 +74,7 @@ if ingredients_list:
     insert_stmt = f"""INSERT INTO SMOOTHIES.PUBLIC.ORDERS (ingredients, name_on_order, order_ts)
     VALUES ('{ingredients_string}', '{name_on_order}', CURRENT_TIMESTAMP())
 
+
     
     time_to_insert = st.button('Submit Order')
 
