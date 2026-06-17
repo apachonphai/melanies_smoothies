4cd2ba456b9ed817f3488db068a3e9a3bfaed251
apachonphai <apachonphai@networkdistribution.com>
2026-06-17 09:16:46 -0500

    Update Streamlit app to display data as dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,8 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df=  st.dataframe(data=smootiefroot_response.json(), use_container_width=True)
 
 
     
